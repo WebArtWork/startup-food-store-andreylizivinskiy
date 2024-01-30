ca6448340d3221acbb36e7de58ba6dcb7e8be9ab
lizvinskyi <andriilizvinskyi@gmail.com>
2024-01-30 16:59:07 +0200

    add modal

--- js/index.js
+++ js/index.js
@@ -23,6 +23,33 @@ $(document).ready(function(){
       ]
     });
 });
+jQuery(($) => {
+  $('.select').on('click', '.select__head', function () {
+      if ($(this).hasClass('open')) {
+          $(this).removeClass('open');
+          $(this).next().fadeOut();
+      } else {
+          $('.select__head').removeClass('open');
+          $('.select__list').fadeOut();
+          $(this).addClass('open');
+          $(this).next().fadeIn();
+      }
+  });
+
+  $('.select').on('click', '.select__item', function () {
+      $('.select__head').removeClass('open');
+      $(this).parent().fadeOut();
+      $(this).parent().prev().text($(this).text());
+      $(this).parent().prev().prev().val($(this).text());
+  });
+
+  $(document).click(function (e) {
+      if (!$(e.target).closest('.select').length) {
+          $('.select__head').removeClass('open');
+          $('.select__list').fadeOut();
+      }
+  });
+});
 $(".burger-wrap").click(function () {
 $(".header-wrap__navigation").toggleClass("header-wrap__navigation--open");
 $(".burger").toggleClass('burger--close');
